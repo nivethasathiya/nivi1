85079c4b5e5e8b66efb8c7816ab8b7846d4bddd8
nivethasathiya <53109733+nivethasathiya@users.noreply.github.com>
2019-07-25 17:42:57 +0530

    Create nivi1.py

---
+++ nivi1.py
@@ -0,0 +1,7 @@
+char = input("Enter a character ")
+
+if(char=='A' or char=='a' or char=='E' or char =='e' or char=='I'
+ or char=='i' or char=='O' or char=='o' or char=='U' or char=='u'):
+    print(char, "is a Vowel")
+else:
+    print(char, "is a Consonant")
